871e96958da9eb098534b2bc50f69baa59e7566f
Ijax claw <inammenziwa074@gmail.com>
2024-06-18 18:55:02 +0200

    4th commit

--- djangorestsite/settings.py
+++ djangorestsite/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 import dj_database_url
-
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -122,7 +122,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-# Default primary key field type
+if not DEBUG:    
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+    
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
-
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
