dafd5abfbe7e62d989341396fd8478b4c7aec85f
Ijax claw <inammenziwa074@gmail.com>
2024-06-18 19:00:36 +0200

    6th commit

--- djangorestsite/settings.py
+++ djangorestsite/settings.py
@@ -122,9 +122,8 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-if not DEBUG:    
-    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
     
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
