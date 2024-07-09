6ef1529d8ae7eb558215811e19df66269b3bfd19
Ijax claw <inammenziwa074@gmail.com>
2024-07-09 17:49:00 +0200

    changes from the first commit

--- djangorestsite/settings.py
+++ djangorestsite/settings.py
@@ -77,10 +77,15 @@ WSGI_APPLICATION = 'djangorestsite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'jelly004',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
+
 
 
 # Password validation
--- restapp/urls.py
+++ restapp/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('data/', views.Blogcreation.as_view(), name="blogcreation"),
-    path('', views.Homepage, name="index")
+    path('', views.Homepage, name="index"),
 ]
